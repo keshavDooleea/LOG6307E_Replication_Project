f5dd104698a49678bae0393764285da1b4e6afa7
keshav <kdooleea@yahoo.ca>
2023-10-21 17:00:56 -0400

    feat: applied pagination to fetch all repos

--- Dataset_Mining/src/main.py
+++ Dataset_Mining/src/main.py
@@ -2,7 +2,9 @@ from steps.repository_collection.main import execute_repositories_collection
 from helpers.git import GitHelper
 
 if __name__ == "__main__":
-    repos = execute_repositories_collection()
+
+    org = "Mirantis"
+    repos = execute_repositories_collection(org)
 
     for repo in repos:
         print(GitHelper.get_repo_details(repo))
