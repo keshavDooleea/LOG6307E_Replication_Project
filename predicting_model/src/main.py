26155045a5346bfc586d7ccbd3acda9b6f942543
ming-xiao-yuan <ming-xiao.yuan@outlook.com>
2023-10-21 13:01:42 -0400

    add data_out and data_in files

--- predicting_model/src/main.py
+++ predicting_model/src/main.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 import pandas as pd
 import numpy as np
 from sklearn.decomposition import PCA
@@ -8,61 +7,83 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import tree
-from sklearn.svm import SVC
+from typing import List, Dict
+
+# file that computes RQ3 requisites, i.e. log transformation, PCA , and 5 original classifiers
+# (CART, KNN, LR, NB, RF) on 4 datasets
+# also computes the classification results for one extra classifier "Support Vector Machine" or "SVM"
+# also computes the feature importance for RQ1
+# inputs:
+# expects to find the authors' dataset files as "MIR.csv", "MOZ.csv", "OST.csv", and "WIK.csv"
+# ouputs:
+# "{metric_name}_median.csv" for four metrics ('precision', 'recall', 'f1', 'roc_auc')
+# that each contains median of 10-fold cross-validation results for the all datasets
 
 
-def read_data(filenames):
-    dfs = [pd.read_csv(filename) for filename in filenames]
-    return dfs
+def main() -> None:
+    mirantis_df: pd.DataFrame = pd.read_csv("../data_in/IST_MIR.csv")
+    mozilla_df: pd.DataFrame = pd.read_csv("../data_in/IST_MOZ.csv")
+    openstack_df: pd.DataFrame = pd.read_csv("../data_in/IST_OST.csv")
+    wikimedia_df: pd.DataFrame = pd.read_csv("../data_in/IST_WIK.csv")
 
+    dataset: List[pd.DataFrame] = [mirantis_df, mozilla_df, openstack_df, wikimedia_df]
+    columns: List[str] = [
+        "org",
+        "file_",
+        "URL",
+        "File",
+        "Lines_of_code",
+        "Require",
+        "Ensure",
+        "Include",
+        "Attribute",
+        "Hard_coded_string",
+        "Comment",
+        "Command",
+        "File_mode",
+        "SSH_KEY",
+        "defect_status",
+    ]
 
-def apply_log_and_pca(df, pca_components):
-    y = df["defect_status"]
-    df_log = df.iloc[:, 4:16].apply(lambda x: np.log(x + 1))
-    pca = PCA(n_components=pca_components)
-    df_pca = pca.fit_transform(df_log)
-    return df_pca, y
+    pcas: List[int] = [6, 8, 7, 7]
+    metrics: List[str] = ["precision", "recall", "f1", "roc_auc"]
 
+    # To store explained variance ratios for each dataset
+    explained_var_ratios: List[np.ndarray] = []
 
-def evaluate_model(model, X, y, metric):
-    scores = cross_val_score(model, X, y, cv=10, scoring=metric)
-    return round(np.median(scores), 3)
+    for metric in metrics:
+        results: Dict[str, Dict[str, float]] = {}
+        for index, df in enumerate(dataset):
+            org_dict: Dict[str, float] = {}
+            y: pd.Series = df[columns[-1]]
+            # log transformation
+            df_log: pd.DataFrame = df[columns[2:14]].apply(lambda x: np.log(x + 1))
+            # PCA transformation (dimensionality reduction)
+            pca = PCA(n_components=pcas[index])
+            df_pca: np.ndarray = pca.fit_transform(df_log)
 
+            pca_for_variance = PCA(n_components="mle", svd_solver="full")
+            pca_for_variance.fit(df_log)
+            print(pca_for_variance.explained_variance_ratio_)
+            explained_var_ratios.append(pca_for_variance.explained_variance_ratio_)
+            # we then performed the variance cover anakysis manually as it was presented in our presentation
+            for model_name, model in [
+                ("CART", tree.DecisionTreeClassifier()),
+                ("KNN", KNeighborsClassifier()),
+                ("LR", LogisticRegression(solver="lbfgs")),
+                ("NB", GaussianNB()),
+                ("RF", RandomForestClassifier(random_state=0)),
+            ]:
+                scores: np.ndarray = cross_val_score(
+                    model, df_pca, y, cv=10, scoring=metric
+                )
+                org_dict[model_name] = round(np.median(scores), 3)
 
-def evaluate_all_models(dfs, pca_components_list, metrics):
-    metric_results = defaultdict(dict)
+            results[df.iloc[0]["org"]] = org_dict
 
-    for df, pca_components in zip(dfs, pca_components_list):
-        org = df.iloc[0]["org"]
-        print(f"Evaluating models for {org}...")
-        X, y = apply_log_and_pca(df, pca_components)
-
-        for metric in metrics:
-            model_scores = {
-                "CART": evaluate_model(tree.DecisionTreeClassifier(), X, y, metric),
-                "KNN": evaluate_model(KNeighborsClassifier(), X, y, metric),
-                "LR": evaluate_model(LogisticRegression(solver="lbfgs"), X, y, metric),
-                "NB": evaluate_model(GaussianNB(), X, y, metric),
-                "RF": evaluate_model(
-                    RandomForestClassifier(random_state=0), X, y, metric
-                ),
-            }
-
-            metric_results[metric][org] = model_scores
-
-    for metric, data in metric_results.items():
-        df_metric = pd.DataFrame.from_dict(data).T
-        df_metric.to_csv(f"{metric}_median.csv")
+        df_out: pd.DataFrame = pd.DataFrame.from_dict(results).T
+        df_out.to_csv(f"../data_out/{metric}_results.csv")
 
 
 if __name__ == "__main__":
-    filenames = [
-        "../data_in/IST_MIR.csv",
-        "../data_in/IST_MOZ.csv",
-        "../data_in/IST_OST.csv",
-        "../data_in/IST_WIK.csv",
-    ]
-    dfs = read_data(filenames)
-    pca_components_list = [6, 8, 7, 7]
-    metrics = ["precision", "recall", "f1", "roc_auc"]
-    evaluate_all_models(dfs, pca_components_list, metrics)
+    main()
