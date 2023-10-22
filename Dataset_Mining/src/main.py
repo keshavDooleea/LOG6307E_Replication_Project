c65dbca0532cd0d10f6b5dcc3f6ee26471c67c6b
keshav <kdooleea@yahoo.ca>
2023-10-22 08:57:57 -0400

    feat: added another file to read from jsons

--- Dataset_Mining/src/main.py
+++ Dataset_Mining/src/main.py
@@ -1,4 +1,5 @@
-from steps.repository_collection.main import RepositoryCollection
+from steps.repository_collection.repositories_via_api import RepositoryCollectionViaAPI
+from steps.repository_collection.repositories_via_json import RepositoryCollectionViaJSON
 from helpers.request import RequestHelper
 
 if __name__ == "__main__":
@@ -6,5 +7,6 @@ if __name__ == "__main__":
     rate_limit = RequestHelper.get_rate_limit()
     print(rate_limit)
 
-    repo_collection = RepositoryCollection()
+    # repo_collection = RepositoryCollectionViaAPI()
+    repo_collection = RepositoryCollectionViaJSON()
     repo_collection.create_dataset()
